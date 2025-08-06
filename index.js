f007bab13bbd857bbe855b49e2cfff5133ae0854
HuyKaiser <accvia2k99@gmail.com>
2025-08-06 16:58:41 +0700

    fix name array regions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Main exports for tikwm-api package
+// Main exports for tiktok-hks package
 const Tiktok = require('./src/Tiktok');
 const TiktokApiServiceProvider = require('./src/TiktokApiServiceProvider');
 const TiktokApiFacade = require('./src/Facades/TiktokApiFacade');
